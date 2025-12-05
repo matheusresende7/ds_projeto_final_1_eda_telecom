00ec2204026c7072327000c5b9b6cd2b9acbfce6
Matheus Resende <75645023+matheusresende7@users.noreply.github.com>
2025-12-05 16:24:18 -0300

    v1.0

---
+++ params/consts.py
@@ -0,0 +1,15 @@
+# Data
+DATASET_RAW = '../data/01_resource/dataset_raw.csv'
+
+DATASET_RAW_COMPRESSED_PANDAS = '../data/02_bronze/dataset_raw_compressed_pandas.csv.gz'
+DATASET_RAW_COMPRESSED_PYSPARK = '../data/02_bronze/dataset_raw_compressed_pyspark'
+
+DATASET_CLEAN_PANDAS = '../data/03_silver/dataset_clean_pandas.parquet'
+DATASET_CLEAN_PYSPARK = '../data/03_silver/dataset_clean_pyspark'
+
+# Reports
+EDA_0 = '../reports/eda_0.html'
+EDA_1 = '../reports/eda_1.html'
+
+# Values
+RANDOM_STATE = 42
